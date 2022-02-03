55cf9b37d0a6b227417f72f42b6a49779bc1b49f
LAPTOP-3Q7N09F8\saibh <sai.bhavana0907@gmail.com>
2022-02-03 17:44:55 -0500

    Home page and basic FE functionality implemented

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { LoginComponent } from './login/login.component';
+
+var routes: Routes = [
+  // {path:'',component: HomePageComponent},
+  {path:'',component: HomePageComponent},
+  {path:'how-it-works',component: HowItWorksComponent},
+  {path:'login',component: LoginComponent},      
+  {path:'pricing',component: HomePageComponent}
+//   {path:'dashboard',
+//   component: HomePageComponent,
+//   children:[
+//     {path:'',component: HomePageComponent},
+//     {path:'how-it-works',component: HowItWorksComponent},
+//     {path:'login',component: LoginComponent},      
+//     {path:'pricing',component: HomePageComponent}
+//   ]
+// }
+]; 
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

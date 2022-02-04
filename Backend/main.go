ee32bb775c82670707408f8e70e7dafa8e7066a6
sivapraneeth000 <sivapraneeth000@gmail.com>
2022-02-04 11:34:29 -0500

    Booking and pricing functionality phase 1 implemented with mockup data.

--- Backend/main.go
+++ Backend/main.go
@@ -13,7 +13,7 @@ import (
 // Book struct (Model)
 type Book struct {
 	Triptype     string  `json:"triptype"`
-	checkinbags   string  `json:"checkinbags"`
+	Checkinbags   string  `json:"checkinbags"`
 	Source string  `json:"source"`
 	Destination string `json:"destination"`
 	Arrival string `json:"arrival"`
@@ -25,22 +25,41 @@ Finalprice string `json:"finalprice"`
 Luggagecollectiondate string `json:"luggagecollectiondate"`
 Courrierpartner string `json:"courrierpartner"`
 }
+type Price1 struct{
+	Numberofbags string `json:"Number of bags"`
+	Price string `json:"price"`
+	Dimensions string `json:"dimensions"`
+	}
+	type Price2 struct{
+		Numberofbags string `json:"Number of bags"`
+		Price string `json:"price"`
+		Dimensions string `json:"dimensions"`
+		}
+		type Price3 struct{
+			Numberofbags string `json:"Number of bags"`
+			Price string `json:"price"`
+			Dimensions string `json:"dimensions"`
+			}
 
-// Author struct
+
+
 
 
 // Init books var as a slice Book struct
 var books []Book
 var replay []Reply
+var a []Price1
+var b []Price2
+var c []Price3
 
-// Get all books
-func getBooks(w http.ResponseWriter, r *http.Request) {
+// Get all bookings
+func getBookings(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
 
-// Get single book
-func getBook(w http.ResponseWriter, r *http.Request) {
+// Get single booking
+func getBooking(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) // Gets params
 	// Loop through books and find one with the id from the params
@@ -53,8 +72,8 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Book{})
 }
 
-// Add new book
-func createBook(w http.ResponseWriter, r *http.Request) {
+// Add new booking
+func createBooking(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var book Book
 	_ = json.NewDecoder(r.Body).Decode(&book)
@@ -63,8 +82,8 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(book)
 }
 
-// Update book
-func updateBook(w http.ResponseWriter, r *http.Request) {
+// Update booking
+func updateBooking(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range books {
@@ -80,8 +99,8 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Delete book
-func deleteBook(w http.ResponseWriter, r *http.Request) {
+// Delete booking
+func deleteBooking(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range books {
@@ -92,16 +111,26 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(books)
 }
-func replyback(w http.ResponseWriter,r *http.Request){
+func replyback(w http.ResponseWriter,r *http.Request) string{
 	w.Header().Set("Content-Type","application/json")
 	params:=mux.Vars(r)
 	for _, item := range books {
-		if item.Custid == params["custid"] {
+		if item.Checkinbags == params["checkinbags"] {
 			json.NewEncoder(w).Encode(item)
-			return
+			if(item.Checkinbags=="1"){
+				return "10$"
+			}
+			if(item.Checkinbags=="2"){
+				return "20$"
+			}
+			if(item.Checkinbags=="3"){
+				return "30$"
+			}
+			
 		}
 	
 }
+return ""
 }
 func getResults(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -119,6 +148,21 @@ func getResult(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(&Reply{})
 }
+func getprice1(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(a)
+	return
+		}
+		func getprice2(w http.ResponseWriter,r *http.Request){
+			w.Header().Set("Content-Type","application/json")
+			json.NewEncoder(w).Encode(b)
+			return
+				}
+				func getprice3(w http.ResponseWriter,r *http.Request){
+					w.Header().Set("Content-Type","application/json")
+					json.NewEncoder(w).Encode(c)
+					return
+						}
 
 
 // Main function
@@ -127,19 +171,30 @@ func main() {
 	r := mux.NewRouter()
 
 	// Hardcoded data - @todo: add database
-	books = append(books, Book{Triptype:"One-way",Custid: "John01", checkinbags:"3", Source: "Florida", Destination: "Alabama", Arrival:"03-04-22"})
-	books = append(books, Book{Triptype:"One-way",Custid: "John02", checkinbags:"2", Source: "New York", Destination: "Tennessee",Arrival:"02-02-22"})
-	replay = append(replay, Reply{Custid: "John01", Finalprice:"35$", Luggagecollectiondate: "03-01-22", Courrierpartner: "FED-EX"})
-	replay = append(replay, Reply{Custid: "John02", Finalprice:"30$", Luggagecollectiondate: "01-29-22", Courrierpartner: "UPS"})
+	books = append(books, Book{Triptype:"One-way",Custid: "John01", Checkinbags:"3", Source: "Florida", Destination: "Alabama", Arrival:"03-04-22"})
+	books = append(books, Book{Triptype:"One-way",Custid: "John02", Checkinbags:"2", Source: "New York", Destination: "Tennessee",Arrival:"02-02-22"})
+	replay = append(replay, Reply{Custid: "John01", Finalprice:"30$", Luggagecollectiondate: "03-01-22", Courrierpartner: "FED-EX"})
+	replay = append(replay, Reply{Custid: "John02", Finalprice:"20$", Luggagecollectiondate: "01-29-22", Courrierpartner: "UPS"})
+	a=append(a,Price1{Numberofbags:"One bag only",Price:"10$(Ten Dollars)",Dimensions:"Maximum allowable 22*14 (inches)"})
+	b=append(b,Price2{Numberofbags:"Two bags only",Price:"20$(Twenty Dollars)",Dimensions:"Maximum allowable 25*16 (inches)"})
+	c=append(c,Price3{Numberofbags:"Three bags only",Price:"30$(Thirty Dollars)",Dimensions: "Maximum allowable 29*18 (inches)"})
+	
+	
+
 
 	// Route handles & endpoints
-	r.HandleFunc("/books", getBooks).Methods("GET")
-	r.HandleFunc("/books/{custid}", getBook).Methods("GET")
-	r.HandleFunc("/books", createBook).Methods("POST")
-	r.HandleFunc("/books/{custid}", updateBook).Methods("PUT")
-	r.HandleFunc("/books/{custid}", deleteBook).Methods("DELETE")
-	r.HandleFunc("/reply", getResults).Methods("GET")
-	r.HandleFunc("/reply/{custid}", getResult).Methods("GET")
+	r.HandleFunc("/bookings", getBookings).Methods("GET")
+	r.HandleFunc("/bookings/{custid}", getBooking).Methods("GET")
+	r.HandleFunc("/bookings", createBooking).Methods("POST")
+	r.HandleFunc("/bookings/{custid}", updateBooking).Methods("PUT")
+	r.HandleFunc("/bookings/{custid}", deleteBooking).Methods("DELETE")
+	r.HandleFunc("/receipt", getResults).Methods("GET")
+	r.HandleFunc("/receipt/{custid}", getResult).Methods("GET")
+	// r.HandleFunc("/checkin/{checkinbags}",replyback)
+	r.HandleFunc("/luggages/one", getprice1).Methods("GET")
+	r.HandleFunc("/luggages/two", getprice2).Methods("GET")
+	r.HandleFunc("/luggages/three", getprice3).Methods("GET")
+
 
 
 	// Start server

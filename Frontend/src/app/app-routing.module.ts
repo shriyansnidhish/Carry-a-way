9a16d61d1427126ee9c0de6ce997ae2987e43cb5
LAPTOP-3Q7N09F8\saibh <sai.bhavana0907@gmail.com>
2022-04-20 17:50:17 -0400

    New component Booking-Options added to display the available booking options for sending the lugagge. Css changes in Homepage component were added to change the layouts. Routes were added as necessary in the app-routing-module and component changes into the app-module.

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BagCalculatorComponent } from './bag-calculator/bag-calculator.component';
+import { BookingOptionsComponent } from './booking-options/booking-options.component';
+import { HelpComponent } from './help/help.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { LoginComponent } from './login/login.component';
@@ -12,16 +14,9 @@ var routes: Routes = [
   {path:'how-it-works',component: HowItWorksComponent},
   {path:'login',component: LoginComponent},      
   {path:'pricing',component: PricingComponent},
-  {path:'bag-calculator',component: BagCalculatorComponent}
-//   {path:'dashboard',
-//   component: HomePageComponent,
-//   children:[
-//     {path:'',component: HomePageComponent},
-//     {path:'how-it-works',component: HowItWorksComponent},
-//     {path:'login',component: LoginComponent},      
-//     {path:'pricing',component: HomePageComponent}
-//   ]
-// }
+  {path:'bag-calculator',component: BagCalculatorComponent},
+  {path:'booking-options',component:BookingOptionsComponent},
+  {path:'help',component:HelpComponent}
 ]; 
 
 
